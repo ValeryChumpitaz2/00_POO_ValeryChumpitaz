6afa1ce2d1a89d8c71fcbbb95d6becac6f8e516f
ValeryChumpitaz2 <valerychumpitaz2@gmail.com>
2026-08-12 09:07:00 -0500

    main

--- AgendaContactos/src/main/java/vallegrande/edu/pe/app/Main.java
+++ AgendaContactos/src/main/java/vallegrande/edu/pe/app/Main.java
@@ -1,14 +1,40 @@
 package vallegrande.edu.pe.app;
 
+import vallegrande.edu.pe.controller.AgendaController;
 import vallegrande.edu.pe.model.Contacto;
+import vallegrande.edu.pe.view.AngendaView;
 
 public class Main {
     public static void main(String[] args) {
-        //Crear objeto
-        Contacto contacto = new Contacto();
-        //Llamar métodos
-        contacto.registrarContacto();
-        contacto.mostrarContacto();
-        contacto.mostrarDireccion();
-}
+        //Crear los componentes
+        AngendaController controller = new AgendaController();
+        AngendaView view = new AngendaView();
+
+        //Mostrar Informacion
+        view.mostrarTitulo();
+
+        //Crear contactos
+        Contacto contacto1 = new Contacto(
+                1,
+                "Ana",
+                "Torres",
+                "Cañete",
+                "983745656",
+                "ana@gmail.com"
+        );
+        Contacto contacto2 = new Contacto(
+                2,
+                "Carlos",
+                "Perez",
+                "Imperial",
+                "951264456",
+                "carlos@gmail.com"
+        );
+        //Agregar contactos
+         controller.agregarContacto(contacto1);
+         controller.agegarContacto(contacto2);
+         //Listar Contactos
+        controller.listarContactos();
+    }
+
 }
