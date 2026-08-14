f88fc9f7ccc6896ffab767b1c7e33adb6acf54c6
ValeryChumpitaz2 <valerychumpitaz2@gmail.com>
2026-08-14 11:30:50 -0500

    atributos y constructor del modelo

---
+++ SistemaBiblioteca/src/main/java/vallegrande/edu/pe/model/Libro.java
@@ -0,0 +1,21 @@
+package vallegrande.edu.pe.model;
+
+public class Libro {
+
+    // ATRIBUTOS
+    private int id;
+    private String titulo;
+    private String autor;
+    private int anio;
+
+    //CONSTRUCTOR
+    public Libro(int id, String titulo, String autor, int anio){
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anio = anio;
+    }
+
+
+
+}
