5224052536de02b22e04b28e5e63a22aae4203bb
ValeryChumpitaz2 <valerychumpitaz2@gmail.com>
2026-09-02 09:20:04 -0500

    Main

--- MiPerfil/src/main/java/vallegrande/edu/pe/miperfil/Main.java
+++ MiPerfil/src/main/java/vallegrande/edu/pe/miperfil/Main.java
@@ -1,9 +1,28 @@
 package vallegrande.edu.pe.miperfil;
 
-import javafx.application.Application;
 
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import vallegrande.edu.pe.miperfil.controller.PerfilController;
+import vallegrande.edu.pe.miperfil.view.PerfilView;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage){
+        PerfilView view = new PerfilView();
+        new PerfilController(view);
+        Scene scene = new Scene(
+                view.getContenedor(),
+                400,
+                400
+        );
+        stage.setTitle("Mi perfil");
+        stage.setScene(scene);
+        stage.show();
     }
+    public static void main(String[] args){
+        launch(args);
+    }
+
 }
